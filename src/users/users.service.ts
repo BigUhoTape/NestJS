8822e1033229330d79ac72d85d1e5e31f7416b8c
Ivan Vostrikov <ivanvostrikov@MacBook-Air-Ivan.local>
2022-08-31 18:08:05 +0700

    create and get users

--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -1,4 +1,21 @@
 import { Injectable } from '@nestjs/common';
+import {User} from "./users.model";
+import {InjectModel} from "@nestjs/sequelize";
+import {CreateUserDto} from "./dto/create-user.dto";
 
 @Injectable()
-export class UsersService {}
+export class UsersService {
+
+    constructor(@InjectModel(User) private userModel: typeof User) {}
+
+    async createUser (dto: CreateUserDto) {
+        const user = await this.userModel.create(dto);
+        return user;
+    }
+
+    async getAllUsers () {
+        const users = await this.userModel.findAll();
+        return users;
+    }
+
+}
