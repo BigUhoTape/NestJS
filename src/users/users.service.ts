3f7972ad33a787b37cd19094bfff643d09941c1e
Ivan Vostrikov <ivanvostrikov@MacBook-Air-Ivan.local>
2022-10-05 20:05:08 +0700

    login and registration via jwt&bcrypt

--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -16,12 +16,22 @@ export class UsersService {
         const user = await this.userModel.create(dto);
         const role = await this.roleService.getRoleByValue('USER');
         await user.$set('roles', [role.id]);
+        user.roles = [role];
         return user;
     }
 
     async getAllUsers () {
         const users = await this.userModel.findAll({include: {all: true}});
         return users;
+    }
+
+    async getUserByEmail (email: string) {
+        const user = await this.userModel.findOne({
+            where: {email},
+            include: {all: true}
+        });
+
+        return user;
     }
 
 }
